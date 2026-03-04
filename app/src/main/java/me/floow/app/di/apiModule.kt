fe3da7db7fd3940a75ce47e7617b9ffe994b09c4
Богдан <bogdan@MacBook-Air-Bogdan.local>
2026-03-04 19:00:42 +0300

    refactor(infra): move realtime-specific app wiring out of infra base

--- app/src/main/java/me/floow/app/di/apiModule.kt
+++ app/src/main/java/me/floow/app/di/apiModule.kt
@@ -1,29 +1,21 @@
 package me.floow.app.di
 
 import me.floow.api.AuthApiImpl
-import me.floow.api.BumpApiImpl
 import me.floow.api.CategoriesApiImpl
 import me.floow.api.CommentsApiImpl
 import me.floow.api.FeedApiImpl
-import me.floow.api.NotificationsApiImpl
-import me.floow.api.NotificationsRealtimeApiImpl
 import me.floow.api.PostsApiImpl
 import me.floow.api.ProfileApiImpl
-import me.floow.api.PushTokensApiImpl
 import me.floow.api.UploadsApiImpl
 import me.floow.api.UsersApiImpl
 import me.floow.api.util.HttpClientProvider
 import me.floow.auth.GoogleOAuthImpl
 import me.floow.domain.api.AuthApi
-import me.floow.domain.api.BumpApi
 import me.floow.domain.api.CategoriesApi
 import me.floow.domain.api.CommentsApi
 import me.floow.domain.api.FeedApi
-import me.floow.domain.api.NotificationsApi
-import me.floow.domain.api.NotificationsRealtimeApi
 import me.floow.domain.api.PostsApi
 import me.floow.domain.api.ProfileApi
-import me.floow.domain.api.PushTokensApi
 import me.floow.domain.api.UploadsApi
 import me.floow.domain.api.UsersApi
 import me.floow.domain.auth.GoogleOAuth
@@ -31,17 +23,13 @@ import org.koin.dsl.module
 
 val apiModule = module {
     single<HttpClientProvider> { HttpClientProvider() }
-    factory<BumpApi> { BumpApiImpl(get(), get(), get(), get()) }
     factory<ProfileApi> { ProfileApiImpl(get(), get(), get(), get()) }
     factory<AuthApi> { AuthApiImpl(get(), get(), get()) }
     factory<UsersApi> { UsersApiImpl(get(), get(), get(), get()) }
     factory<PostsApi> { PostsApiImpl(get(), get(), get(), get()) }
     factory<CategoriesApi> { CategoriesApiImpl(get(), get(), get(), get()) }
-	factory<CommentsApi> { CommentsApiImpl(get(), get(), get(), get()) }
-	factory<FeedApi> { FeedApiImpl(get(), get(), get(), get()) }
-	factory<NotificationsApi> { NotificationsApiImpl(get(), get(), get(), get()) }
-	factory<NotificationsRealtimeApi> { NotificationsRealtimeApiImpl(get(), get(), get(), get()) }
-	factory<PushTokensApi> { PushTokensApiImpl(get(), get(), get(), get()) }
-	factory<UploadsApi> { UploadsApiImpl(get(), get(), get(), get()) }
-	factory<GoogleOAuth> { GoogleOAuthImpl(get(), get()) }
-}
+	    factory<CommentsApi> { CommentsApiImpl(get(), get(), get(), get()) }
+	    factory<FeedApi> { FeedApiImpl(get(), get(), get(), get()) }
+	    factory<UploadsApi> { UploadsApiImpl(get(), get(), get(), get()) }
+	    factory<GoogleOAuth> { GoogleOAuthImpl(get(), get()) }
+	}
--- app/src/main/java/me/floow/app/di/dataModule.kt
+++ app/src/main/java/me/floow/app/di/dataModule.kt
@@ -1,39 +1,27 @@
 package me.floow.app.di
 
 import me.floow.app.debug.SharedPrefsMockStorage
-import me.floow.data.repos.BumpRepositoryImpl
 import me.floow.data.repos.FeedRepositoryImpl
 import me.floow.data.repos.CategoryCatalogRepositoryImpl
 import me.floow.data.repos.CommentsRepositoryImpl
-import me.floow.data.repos.NotificationsRepositoryImpl
-import me.floow.data.repos.NotificationsRealtimeRepositoryImpl
 import me.floow.data.repos.PostsRepositoryImpl
 import me.floow.data.repos.ProfileRepositoryImpl
-import me.floow.data.repos.PushTokensRepositoryImpl
 import me.floow.data.repos.UploadsRepositoryImpl
 import me.floow.data.repos.UserProfileRepositoryImpl
 import me.floow.data.repos.UsersRepositoryImpl
 import me.floow.domain.data.repos.FeedRepository
-import me.floow.domain.data.repos.BumpRepository
 import me.floow.domain.data.repos.CategoryCatalogRepository
 import me.floow.domain.data.repos.CommentsRepository
-import me.floow.domain.data.repos.NotificationsRepository
-import me.floow.domain.data.repos.NotificationsRealtimeRepository
 import me.floow.domain.data.repos.PostsRepository
 import me.floow.domain.data.repos.ProfileRepository
-import me.floow.domain.data.repos.PushTokensRepository
 import me.floow.domain.data.repos.UploadsRepository
 import me.floow.domain.data.repos.UserProfileRepository
 import me.floow.domain.data.repos.UsersRepository
 import me.floow.mock.data.MockFeedRepository
-import me.floow.mock.data.MockBumpRepository
 import me.floow.mock.data.MockCategoryCatalogRepository
 import me.floow.mock.data.MockCommentsRepository
-import me.floow.mock.data.MockNotificationsRepository
-import me.floow.mock.data.MockNotificationsRealtimeRepository
 import me.floow.mock.data.MockPostsRepository
 import me.floow.mock.data.MockProfileRepository
-import me.floow.mock.data.MockPushTokensRepository
 import me.floow.mock.data.MockUploadsRepository
 import me.floow.mock.data.MockUserProfileRepository
 import me.floow.mock.data.MockUsersRepository
@@ -42,7 +30,6 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val dataModule = module {
-	single<BumpRepository> { BumpRepositoryImpl(get()) }
 	single<ProfileRepository> { ProfileRepositoryImpl(get(), get()) }
 	single<UserProfileRepository> { UserProfileRepositoryImpl(get()) }
 	single<CategoryCatalogRepository> { CategoryCatalogRepositoryImpl(get(), get()) }
@@ -50,15 +37,11 @@ val dataModule = module {
 	single<UsersRepository> { UsersRepositoryImpl(get(), get()) }
 	single<PostsRepository> { PostsRepositoryImpl(get(), get()) }
 	single<CommentsRepository> { CommentsRepositoryImpl(get(), get()) }
-	single<NotificationsRepository> { NotificationsRepositoryImpl(get(), get()) }
-	single<NotificationsRealtimeRepository> { NotificationsRealtimeRepositoryImpl(get(), get(), get(), get(), get()) }
-	single<PushTokensRepository> { PushTokensRepositoryImpl(get(), get()) }
 	single<UploadsRepository> { UploadsRepositoryImpl(get(), get()) }
 }
 
 val mockDataModule = module {
     single<MockStorage> { SharedPrefsMockStorage(androidContext()) }
-	single<BumpRepository> { MockBumpRepository() }
 	single<ProfileRepository> { MockProfileRepository(get()) }
 	single<UserProfileRepository> { MockUserProfileRepository(get()) }
 	single<CategoryCatalogRepository> { MockCategoryCatalogRepository() }
@@ -66,8 +49,5 @@ val mockDataModule = module {
 	single<UsersRepository> { MockUsersRepository(get()) }
 	single<PostsRepository> { MockPostsRepository() }
 	single<CommentsRepository> { MockCommentsRepository() }
-	single<NotificationsRepository> { MockNotificationsRepository() }
-	single<NotificationsRealtimeRepository> { MockNotificationsRealtimeRepository() }
-	single<PushTokensRepository> { MockPushTokensRepository() }
 	single<UploadsRepository> { MockUploadsRepository() }
 }
